9d6f7893f84a67555044f62ce09e200e23db3942
dark-wander <54247514+dark-wander@users.noreply.github.com>
2021-08-12 16:44:51 -0400

    Create ConnectCheck.ps1

---
+++ ConnectCheck.ps1
@@ -0,0 +1,17 @@
+﻿$targets = Get-Content "//Path of IP address List//"
+$ping = New-Object System.Net.NetworkInformation.Ping
+
+    Out-File "//Output File destination//"
+
+ForEach($target in $targets) { 
+    $output = ($ping.Send($target)).Status
+    Write-Host $target, "-", $output
+
+        If ($output -ne 0){
+        
+         Write-Output $target | Out-File "//Output File destination//" -Append
+
+        
+        }
+}
+Pause
