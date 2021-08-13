1d038c7784c8edcd37058c9ab4400802f2051176
dark-wander <54247514+dark-wander@users.noreply.github.com>
2021-08-13 15:40:39 -0400

    Update ConnectCheck.ps1

--- ConnectCheck.ps1
+++ ConnectCheck.ps1
@@ -1,15 +1,21 @@
-$targets = Get-Content "ip.txt"
+
+Param(
+  [Parameter(Mandatory=$true, position=0)][string]$csvfile
+)
+
+
+$targets = import-csv $csvfile
 $ping = New-Object System.Net.NetworkInformation.Ping
 
 ForEach($target in $targets) { 
-    $output = ($ping.Send($target)).Status
-    Write-Host $target, "-", $output
+    $output = ($ping.Send($target.("IP Address"))).Status
+    Write-Host $target.("IP Address"), "-", $output
 
-        If ($output -ne 0){       
+      <#  If ($output -ne 0){       
          Write-Output $target | Out-File "fail.txt" -Append
         }else{
 	Write-Output $target | Out-File "pass.txt" -Append
-	}    
+	}   #> 
 
 }
-Pause
+
