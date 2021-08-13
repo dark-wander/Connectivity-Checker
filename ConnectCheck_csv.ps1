3426bd394cb9aa602e166be689fe1a8ef62c93ca
dark-wander <54247514+dark-wander@users.noreply.github.com>
2021-08-12 21:32:35 -0400

    Add files via upload
    Importing the CSV does no import as a table bus as a string of values. This issue only exists when calling the CSV object. Any solutions??

---
+++ ConnectCheck_csv.ps1
@@ -0,0 +1,24 @@
+﻿Param(
+  [Parameter(Mandatory=$true, position=0)][string]$csvfile
+)
+
+
+$targets = Import-Csv $csvfile
+Write-Host $targets
+
+    
+ForEach($target in $targets) { 
+    $output = (System.Net.NetworkInformation.Ping.Send($target)).Status
+    Write-Host $target, "-", $output
+
+        
+        If ($output -ne 0){
+        
+         Write-Output $target | Out-File "//Output File destination//" -Append
+
+          }else{
+	Write-Output $target | Out-File "pass.txt" -Append
+        }
+}
+
+Pause
